587be57c4a60257b017016b14b242eec86c2dd64
rankshow <uwaomaobinna20@gmail.com>
2022-10-22 09:34:20 +0100

    initialized a mini book project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-// Nested Component, React tool
-function Greeting() {
-      return( 
-          <div>
-              <Header />
-              < Parahragh />
-              <Time />
-          </div>
-        );
-      };
-
-      // implicit return
- const Header = () => <h1>Church website will be ready soon</h1>;
- const Time = () => <input type="text" name="" id="" />
-//  explicit return
-const Parahragh = () => {
-  return <p>Garbage is the word of the wise never look behind</p>;
+import './index.css';
+// // Nested Component, React tool
+// Css
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
+      <Cost />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img className="pic"
+      src="https://www.sunnewsonline.com/ezoimgfmt/sunnewsonline.com/wp-content/uploads/2021/12/United-Airlines-750x375.jpeg?ezimgfmt=ngcb1/notWebP"
+      alt=""
+    />
+  );
+};
+const Title = () => <h1>Daily news papers</h1>
+const Author = () => <h4>sunnewspaper</h4>
+const Cost = () => <a href="#">$100</a>
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
